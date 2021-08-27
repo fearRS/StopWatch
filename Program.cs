6e41f095378eaefa62fd1260469c0739804cf733
fearRS <arklitereaper@gmail.com>
2021-08-27 11:36:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Watch stopStart = new Watch();
+
+            Console.WriteLine("Press Enter to start or 'q' to quit");
+
+            while (true)
+            {
+                ConsoleKey input = Console.ReadKey().Key;
+                if (input != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("\nPress Enter to start the Stopwatch");
+                }
+                else
+                {
+                    Console.WriteLine("Watch started!\nPress Enter to stop");
+                    stopStart.Start();
+                    input = Console.ReadKey().Key;
+                    while (input != ConsoleKey.Enter)
+                    {
+                        Console.WriteLine("\nPress Enter to start the Stopwatch");
+                        input = Console.ReadKey().Key;
+                    }
+                    stopStart.Stop();
+                    Console.WriteLine($"Time elapsed is: {stopStart.Stop()} seconds.");
+                }
+            }
+
+        }
+    }
+}
---
+++ StopStartTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StopWatch
+{
+    public class Watch
+    {
+        DateTime startTime = DateTime.Now;   //class level variable used in timespan
+        DateTime stopTime = DateTime.Now;
+        public void Start()
+        {
+            startTime = DateTime.Now;   // variable changed before timespan gets to it
+        }
+        public double Stop()
+        {
+            stopTime = DateTime.Now;
+            return Math.Round(Convert.ToDouble((stopTime - startTime).TotalSeconds), 1); ;
+        }
+    }
+}
